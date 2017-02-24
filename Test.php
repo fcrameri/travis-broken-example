33c302360e25cda8a0b71504ac28d627a34966e2
Federico Crameri <federico.crameri@hotmail.it>
2017-02-24 21:17:40 +0100

    now is good!

--- Test.php
+++ Test.php
@@ -2,10 +2,8 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public $assertEquals;
-
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
+		$this->assertEquals(1+1,2);
   	}
 }
 
